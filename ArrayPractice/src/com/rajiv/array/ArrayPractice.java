9fc2850be651f39fe335e77032af8e16d39655f7
rajiv kumar <rajivtech89@gmail.com>
2022-04-04 21:34:15 +0530

    sub array print corrected

--- ArrayPractice/src/com/rajiv/array/ArrayPractice.java
+++ ArrayPractice/src/com/rajiv/array/ArrayPractice.java
@@ -4,14 +4,16 @@ public class ArrayPractice {
 
 	public static void main(String[] args) {
 		
-		int[] numbers = {1,2,3,4,5};
+		int[] numbers = {1,2,3,4};
 		ArrayPractice.printSubArrays(numbers);;
 	}
 
 	public static void printSubArrays(int[] numbers) {
 		for (int i = 0; i < numbers.length; i++) {
-			for (int j = 0; j <= i; j++) {
-				System.out.print(numbers[j]);
+			for (int j = 0; j <numbers.length; j++) {
+				for(int k=i;k<=j;k++) {
+					System.out.println(numbers[k]);
+				}
 			}
 			System.out.println();
 		}
